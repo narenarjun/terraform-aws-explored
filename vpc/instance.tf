c2e5591b97a8fc9dedfee217b4fc1247513f4a5f
Narendran <narendev0610@gmail.com>
2021-01-04 18:13:46 +0530

    additional ebs volume created and attached to ec2 instance and set to presistance

--- vpc/instance.tf
+++ vpc/instance.tf
@@ -12,3 +12,20 @@ resource "aws_instance" "vc-example-1" {
   key_name = aws_key_pair.magickeypair.key_name
 
 }
+
+# creating a ebs storage volume
+resource "aws_ebs_volume" "ebs-mainvpc-vol-1" {
+  availability_zone = "us-east-1a"
+  size              = 20
+  type              = "gp2"
+  tags = {
+    Name = "Extra data volume"
+  }
+}
+
+# attaching ebs volume to the ec2 instance
+resource "aws_volume_attachment" "ebs-vol-1-attach" {
+  device_name = "/dev/xvdh"
+  volume_id   = aws_ebs_volume.ebs-mainvpc-vol-1.id
+  instance_id = aws_instance.vc-example-1.id
+}
